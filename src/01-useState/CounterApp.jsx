001902b36e0693b2ed7b10a331caa8a753cf3b03
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-22 18:13:33 +0200

    WIP

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
+
 export const CounterApp = () => {
+  const [counter, setCounter] = useState({
+    counter1: 0,
+    counter2: 5,
+    counter3: 10,
+  });
+
+  const plus = () => {
+    setCounter({
+      ...counter,
+      counter1: counter.counter1 + 1,
+    });
+  };
+
+  const minor = () => {
+    setCounter({
+      ...counter,
+      counter1: counter.counter1 - 1,
+    });
+  };
+
   return (
     <div>
-      <div>Counter: </div>
+      <h1>Counter1: {counter.counter1}</h1>
+      <h1>Counter2: {counter.counter2}</h1>
+      <h1>Counter3: {counter.counter3}</h1>
       <hr />
-      <button>+1</button>
-      <button>-1</button>
+      <button className="btn" onClick={plus}>
+        +1
+      </button>
+      <button className="btn" onClick={minor}>
+        -1
+      </button>
     </div>
   );
 };
