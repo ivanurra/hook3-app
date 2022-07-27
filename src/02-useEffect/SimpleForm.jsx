c5629206e25896536ebddc3798540997a6a68997
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-27 16:51:15 +0200

    WIP

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,19 +1,44 @@
+import { useEffect, useState } from "react";
+
 export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    username: "john",
+    email: "john.doe@gmail.com",
+  });
+
+  const { username, email } = formState;
+
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log("useEffect here!");
+  }, []);
+
   return (
     <>
-      <h1>Simple Form</h1>
+      <h1>Form</h1>
       <hr />
       <input
         type="text"
         className="form-control"
         placeholder="Username"
         name="username"
+        value={username}
+        onChange={onInputChange}
       />
       <input
         type="email"
         className="form-control mt-3"
         placeholder="Email"
         name="email"
+        value={email}
+        onChange={onInputChange}
       />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { SimpleForm } from "./02-useEffect/SimpleForm";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <SimpleForm />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <SimpleForm />
+  // </React.StrictMode>
 );
