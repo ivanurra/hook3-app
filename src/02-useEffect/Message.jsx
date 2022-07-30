25e3ebb06e156502c71dfebfeac6cf9f6d672340
Ivan Urra <ivan.urra.wd@gmail.com>
2022-07-30 18:44:49 +0200

    WIP

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -2,10 +2,15 @@ import { useEffect } from "react";
 
 export const Message = () => {
   useEffect(() => {
-    console.log("Message Mounted");
+    const onMouseMove = ({ x, y }) => {
+      const coords = { x, y };
+      console.log(coords);
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
 
     return () => {
-      console.log("Message Unmounted");
+      window.removeEventListener("mousemove", onMouseMove);
     };
   }, []);
 
