7b46a2de284b059457a09325a523af54f98e196a
Ivan Urra <ivan.urra.wd@gmail.com>
2022-08-03 08:17:58 +0200

    WIP

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -5,11 +5,23 @@ export const MultipleCustomHooks = () => {
 
   const { data, isLoading, hasError } = useFetch(url);
 
-  console.log(data, isLoading, hasError);
+  const { author, quote } = !!data && data[0];
+
   return (
     <>
       <h2>Breaking Bad Quotes</h2>
       <hr />
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-right">
+          <p className="mb-1 h3 text-center">{quote}</p>
+          <footer className="blockquote-footer text-center mt-1">
+            {author}
+          </footer>
+        </blockquote>
+      )}
+      <button className="btn btn-primary">Next quote</button>
     </>
   );
 };
