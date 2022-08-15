ecbd04855896d9f44bd024515c8afd46845433eb
Ivan Urra <ivan.urra.wd@gmail.com>
2022-08-15 09:18:34 +0200

    WIP

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+const initialState = [
+  {
+    id: 1,
+    todo: "Buy milk",
+    done: "false",
+  },
+];
+
+const todoReducer = (state = initialState, action = {}) => {
+  if (action.type === "[TODO] add todo") {
+    return [...state, action.payload];
+  }
+  return state;
+};
+
+let todos = todoReducer();
+
+const newTodo = {
+  id: 2,
+  todo: "Buy cookies",
+  done: "false",
+};
+
+const addTodoAction = {
+  type: "[TODO] add todo",
+  payload: newTodo,
+};
+
+todos = todoReducer(todos, addTodoAction);
+
+console.log(todos);
