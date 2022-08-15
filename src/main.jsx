f75c6dafd17afb27089cfc34228826580199760e
Ivan Urra <ivan.urra.wd@gmail.com>
2022-08-15 08:50:40 +0200

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Parent } from "./07-tarea-memo/Parent";
 import "./index.css";
+import "./08-useReducer/intro-reducer";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Parent />);
+// ReactDOM.createRoot(document.getElementById("root")).render(<Parent />);
