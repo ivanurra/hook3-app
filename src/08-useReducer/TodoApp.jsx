405ae85372657ce014b44831dc1174d9c8cabb6a
Ivan Urra <ivan.urra.wd@gmail.com>
2022-08-17 08:20:57 +0200

    WIP

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -21,11 +21,32 @@ export const TodoApp = () => {
     <>
       <h1>TodoApp</h1>
       <hr />
-      <ul>
-        <li>item1</li>
-        <li>item2</li>
-        <li>item2</li>
-      </ul>
+      <div className="row">
+        <div className="col-6">
+          <h4>Pending Tasks (10)</h4>
+          <hr />
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">Item 1</span>
+              <button className="btn btn-danger ml-2">Delete</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <h4>Add ToDo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Write your task"
+              className="form-control"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-2">
+              Add task
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
