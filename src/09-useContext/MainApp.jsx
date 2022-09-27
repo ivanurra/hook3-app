43bcc26435b1bffdfc3a6309395cd4b5910f203f
Ivan Urra <ivan.urra.wd@gmail.com>
2022-09-27 08:50:07 +0200

    routes

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,19 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
-      <hr/>
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
   );
 };
