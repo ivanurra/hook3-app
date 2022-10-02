688f7db98d1fd703f14dbfb52b3eff4cafe92ea1
Ivan Urra <ivan.urra.wd@gmail.com>
2022-10-02 12:36:34 +0200

    WIP

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,13 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <>
-      <h1>MainApp</h1>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/login">Login</Link>
-      <hr />
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+          <span className="pl-3">MainApp</span>
+        </Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-item nav-link" to="/login">
+              LogIn
+            </NavLink>
+            <NavLink className="nav-item nav-link" to="/about">
+              About
+            </NavLink>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
